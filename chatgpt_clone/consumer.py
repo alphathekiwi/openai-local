25369c24d904ded4ccc2c08d0b954dd1527f54f3
alphathekiwi <alphathekiwi@gmail.com>
2024-08-02 16:58:22 +1200

    Initial version of interaction with openAI api

--- chatgpt_clone/consumer.py
+++ chatgpt_clone/consumer.py
@@ -1,17 +1,28 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
+from openai import OpenAI
 
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        self.client = OpenAI()
         self.accept()
 
     def disconnect(self, close_code):
         pass
 
     def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-        self.send(text_data=json.dumps({
-            'message': message
-        }))
+        json_data = json.loads(text_data)
+        msg_id = json_data['msg_id']
+        message = json_data['message']
+        stream = self.client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[{"role": "user", "content": message}],
+            stream=True,
+        )
+        for chunk in stream:
+            if chunk.choices[0].delta.content is not None:
+                self.send(text_data=json.dumps({
+                    'msg_id': msg_id,
+                    'message': chunk.choices[0].delta.content
+                }))
--- chatgpt_clone/static/simple_gpt.js
+++ chatgpt_clone/static/simple_gpt.js
@@ -1,22 +1,38 @@
+let current = 0;
 const chatSocket = new WebSocket('ws://' + window.location.host + '/ws/gpt_local/');
+window.onbeforeunload = function(event) {
+    chatSocket.close();
+};
 
 chatSocket.onmessage = function(e) {
     const data = JSON.parse(e.data);
-    const message = data['message'];
-    // create element
-    const element = document.createElement('div');
-    element.textContent = message;
-    document.getElementById('conversation').appendChild(element);
+    updateMessageElement(data['msg_id'], data['message']);
 };
 
 chatSocket.onclose = function(e) {
     console.error('Chat socket closed unexpectedly');
 };
 
-// Send message to server
 function sendMessage() {
     const message = document.getElementById('message').value;
+    createMessageElement(current, message);
+    createMessageElement(current+1, '', 'robot');
     chatSocket.send(JSON.stringify({
+        'msg_id': current + 1,
         'message': message
     }));
+    current += 2;
+}
+
+function createMessageElement(msg_id, message, role='human') {
+    const element = document.createElement('div');
+    element.textContent = message;
+    element.dataset.role = role;
+    element.dataset.msgId = msg_id;
+    document.getElementById('conversation').appendChild(element);
+}
+function updateMessageElement(msg_id, message) {
+    const element = document.querySelector(`[data-msg-id="${msg_id}"]`);
+    const text = element.textContent
+    element.textContent = text + message;
 }
