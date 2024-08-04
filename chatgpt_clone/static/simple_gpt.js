879a1d7431b7031a1c663846df6d85ab2b103d60
alphathekiwi <alphathekiwi@gmail.com>
2024-08-04 12:42:03 +1200

    Refined md parser and explained its flow of logic in comment

--- chatgpt_clone/static/simple_gpt.js
+++ chatgpt_clone/static/simple_gpt.js
@@ -8,7 +8,7 @@ chatSocket.onmessage = function(e) {
     const data = JSON.parse(e.data);
     const element = document.querySelector(`[data-msg-id="${data['msg_id']}"]`);
     if (data['completed'] == true) {
-        element.innerHTML = simpleMarkdownCodeBlocksFirst(element.textContent);
+        element.innerHTML = simpleMdpCodeBlocks(element.textContent);
     } else {
         element.textContent += data['message'];
     }
@@ -36,28 +36,47 @@ function createMessageElement(msg_id, message, role='human') {
     element.dataset.msgId = msg_id;
     document.getElementById('conversation').appendChild(element);
 }
-function simpleMarkdownCodeBlocksFirst(text) {
+
+// I did look for a markdown parser library but ultimately decided
+// it would be more interesting to write my own
+// Only some of the extended specs are supported.
+// NOT SUPPORTED: tables, footnotes, emoji, definition lists, heading ids
+//  TODO:  add support for tables
+//  TODO:  add https://highlightjs.org/ for syntax highlighting in code blocks
+//
+//                IS CODE BLOCK:
+//           YES                 NO
+//            |                   |
+//       no further         Run fullline items formatting
+//       formatting               |
+//                          IS INLINE CODE:
+//                        YES             NO
+//                         |               |
+//                    no further       Runn inline items formatting
+
+
+function simpleMdpCodeBlocks(text) {
     let parts = text.split(/```/g);
     for (let i = 0; i < parts.length; i++) {
         if (i % 2 == 0) {
             if (parts.length > 2) {
-                parts[i] = simpleMarkdownInlineCodeNext(parts[i]);
+                parts[i] = simpleMdpInlineCode(simpleMdpFulllineItems(parts[i]));
             }
         } else {
             if (/html/.test(parts[i])) {
                 parts[i] = escapeHTML(parts[i]).replace(/^html/i, '<iframe seamless srcdoc="') + '"></iframe>';
             } else {
-                parts[i] = parts[i].replace(/^(\w\n)?/i, '<code class="code $1">').replace(/\n/g,'<br>') + '</code>';
+                parts[i] = parts[i].replace(/^(\w\n)?/i, '<code class="code $1">') + '</code>';
             }
         }
     }
-    return parts.join('');
+    return parts.join('').replace(/([\deilr])>\n+/g, '$1>').replace(/([^>])\n+/g,'$1<br>');
 }
-function simpleMarkdownInlineCodeNext(text) {
+function simpleMdpInlineCode(text) {
     let pattern = /`/g;
     let indices = [0];
     while ((match = pattern.exec(text)) != null) {
-        if (indices.length % 2 == 1 && /[^\n\r`]+`/.test(text.substring(match.index, text.length))) {
+        if (indices.length % 2 == 1 && /([^\n\r`])+`/.test(text.substring(match.index, text.length))) {
             indices.push(match.index); // only add the index if a newline char is not found
         } else if (indices.length % 2 == 0) {
             indices.push(match.index + 1); // if the start was added, add the end
@@ -68,52 +87,44 @@ function simpleMarkdownInlineCodeNext(text) {
     console.log(indices);
     let newText = '';
     for (let i = 0; i < indices.length; i+=2) {
-        newText += simpleMarkdownParser(text.substring(indices[i], indices[i+1]));
+        newText += simpleMdpInlineItems(text.substring(indices[i], indices[i+1]));
         if (i+2 < indices.length) { // there won't be a closing code block
             newText += '<code class="inline">' + text.substring(indices[i+1], indices[i+2]) + '</code>';
         }
     }
     return newText;
 }
-
-
-function simpleMarkdownParser(text) {
-    // I did look for a markdown parser library but ultimately decided
-    // it would be more interesting to write my own
-    // Only some of the extended specs are supported.
-    // NOT SUPPORTED: tables, footnotes, emoji, definition lists, heading ids
-    //  TODO:  add support for tables
-    //  TODO:  add https://highlightjs.org/ for syntax highlighting in code blocks
-	return text
-		.replace(/^###### (.*)$/gim, '<h4>$1</h6>') // h6 tag
-		.replace(/^##### (.*)$/gim, '<h4>$1</h5>')  // h5 tag
-		.replace(/^#### (.*)$/gim, '<h4>$1</h4>')   // h4 tag
-		.replace(/^### (.*)$/gim, '<h3>$1</h3>')    // h3 tag
-		.replace(/^## (.*)$/gim, '<h2>$1</h2>')     // h2 tag
-		.replace(/^# (.*)$/gim, '<h1>$1</h1>')      // h1 tag
-        .replace(/^> *(.+)$/gim, '<blockquote>\n$1</blockquote>') // blockquote
-        .replace(/<\/blockquote>\n?<blockquote>/gim, '<br>')      // merge adjacent blockquote items
-        .replace(/^ *\- \[ \] (.*)$/gim, '<ul><li><input type="checkbox">$1</li></ul><br>')          // checkbox list
-        .replace(/^ *\- \[x\] (.*)$/gim, '<ul><li><input type="checkbox" checked>$1</li></ul><br>')  // checkbox list ticked
-        .replace(/<\/li><\/ul><br>\n?<ul><li>/gim, '</li><li>')                                      // merge adjacent check list items
-        .replace(/^ *\- +(.*)$/gim, '<ul><li>$1</li></ul>')     // bullet list
-        .replace(/<\/li><\/ul>\n?<ul><li>/gim, '</li><li>')     // merge adjacent bullet list items
-        .replace(/^ *(\d+)\. (.*)$/gim, '<ol><li value="$1">$2</li></ol>') // numbered list
-        .replace(/<\/li><\/ol>\n?<ol><li/gim, '</li><li')                  // merge adjacent numbered list items
-		.replace(/^(_{3}|-{3}|\*{3})$/gim, '<hr>')         // horizontal rule
-		.replace(/~~([^~\n\r]+)~~/gim, '<s>$1</s>')        // strikethrough text
-		.replace(/\*\*([^*\n\r]+)\*\*/gim, '<b>$1</b>')    // bold text
-		.replace(/\*([^*\n\r]+)\*/gim, '<em>$1</em>')      // italic text
-		.replace(/__([^_\n\r]+)__/gim, '<b><i>$1</i></b>') // bold and italic text
-		.replace(/_([^_\n\r]+)_/gim, '<i>$1</i>')          // italic text
-		.replace(/==([^=\n\r]+)==/gim, '<mark>$1</mark>')  // highlighted text
-		.replace(/\^([^\^\n\r]+)\^/gim, '<sup>$1</sup>')   // superscript text
-		.replace(/~([^~\n\r]+)~/gim, '<sub>$1</sub>')      // subscript text
-        .replace(/!\[([^\]\n\r]+)\]\(([^\)\n\r]+)\)/gim, '<img src="$2" alt="$1"></img>') // images
-        .replace(/\[([^\]\n\r]+)\]\(([^\)\n\r]+)\)/gim, '<a href="$2">$1</a>')            // hyperlink
-        .replace(/\n$/gim, '<br>'); // line break
+function simpleMdpFulllineItems(text) {
+	return text.replace(/^# (.*)$/gm, '<h1>$1</h1>') // h1 tag
+        .replace(/^## (.*)$/gm, '<h2>$1</h2>')       // h2 tag
+        .replace(/^### (.*)$/gm, '<h3>$1</h3>')      // h3 tag
+        .replace(/^#### (.*)$/gm, '<h4>$1</h4>')     // h4 tag
+        .replace(/^##### (.*)$/gm, '<h4>$1</h5>')    // h5 tag
+        .replace(/^###### (.*)$/gm, '<h4>$1</h6>')   // h6 tag
+		.replace(/^(_{3}|-{3}|\*{3})$/gm, '<hr>')    // horizontal rule
+        .replace(/^> *(.+)$/gm, '<blockquote>\n$1</blockquote>') // blockquote
+        .replace(/<\/blockquote>\n?<blockquote>/gi, '<br>')      // merge adjacent blockquote items
+        .replace(/^ *\- \[ \] (.*)$/gm, '<ul><li><input type="checkbox">$1</li></ul><br>')         // checkbox list
+        .replace(/^ *\- \[x\] (.*)$/gm, '<ul><li><input type="checkbox" checked>$1</li></ul><br>') // checkbox list ticked
+        .replace(/li><\/ul>(<br>)?\n?<ul><li/gi, 'li><li')                                         // merge adjacent check list items
+        .replace(/^ *\- +(.*)$/gm, '<ul><li>$1</li></ul>') // bullet list
+        .replace(/li><\/ul>(<br>)?\n?<ul><li/gi, 'li><li') // merge adjacent bullet list items
+        .replace(/^ *(\d+)\. (.*)$/gm, '<ol><li value="$1">$2</li></ol>') // numbered list
+        .replace(/li><\/ol>(<br>)?\n?<ol><li/gi, 'li><li');               // merge adjacent numbered list items
 }
-
+function simpleMdpInlineItems(text) {
+    return text.replace(/~~([^~\n\r]+)~~/g, '<s>$1</s>') // strikethrough text
+		.replace(/\*\*([^*\n\r]+)\*\*/g, '<b>$1</b>')    // bold text
+		.replace(/\*([^*\n\r]+)\*/g, '<em>$1</em>')      // italic text
+		.replace(/__([^_\n\r]+)__/g, '<b><i>$1</i></b>') // bold and italic text
+		.replace(/_([^_\n\r]+)_/g, '<em>$1</em>')        // italic text
+		.replace(/==([^=\n\r]+)==/g, '<mark>$1</mark>')  // highlighted text
+		.replace(/\^([^\^\n\r]+)\^/g, '<sup>$1</sup>')   // superscript text
+		.replace(/~([^~\n\r]+)~/g, '<sub>$1</sub>')      // subscript text
+        .replace(/!\[([^\]\n\r]+)\]\(([^\)\n\r]+)\)/g, '<img src="$2" alt="$1"></img>') // images
+        .replace(/\[([^\]\n\r]+)\]\(([^\)\n\r]+)\)/g, '<a href="$2">$1</a>')            // hyperlink
+        .replace(/([^\("`>]) *?(http(s)?:\/\/.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b(?:[-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi, '$1<a href="$2">$2</a>');
+}
 function escapeHTML(text) { 
     return text.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;");
 }
